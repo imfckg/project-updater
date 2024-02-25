8f8cf1151ba22a8dc22d83d5045041ab31b1ec9e
Ivan Migunov <104531564+imfckg@users.noreply.github.com>
2024-02-25 16:40:42 +0300

    project scanner: #3 (#4)

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # PROJECT-UPDATER 
 
+[![codecov](https://codecov.io/gh/imfckg/project-updater/graph/badge.svg?token=CUTBMWOEFT)](https://codecov.io/gh/imfckg/project-updater)
+
 Git project updater
---
+++ src/project_scanner/main.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+import os
+from typing import Iterator
+
+
+class GitProjectScanner:
+    DEFAULT_DEPTH: int = 3
+
+    def __init__(self, path: Path, depth: int = DEFAULT_DEPTH):
+        self.path = path
+        self.depth = depth
+        self.iterator = self.scan(self.path, self.depth)
+
+    def __iter__(self) -> Iterator[str]:
+        yield from self.scan(self.path, self.depth)
+
+    def __next__(self):
+        return next(self.iterator)
+
+    def scan(self, current_path: Path, current_depth: int) -> Iterator[str]:
+        """
+        Осуществляет поиск вложенных Git проектов в заданном каталоге.
+
+        :param current_path: Путь к текущему каталогу
+        :param current_depth: глубина поиска
+        """
+        if current_depth > 0:
+            if '.git' in os.listdir(current_path):
+                yield str(current_path)
+            for entry in os.listdir(current_path):
+                entry_path = current_path / entry
+                if entry_path.is_dir():
+                    yield from self.scan(entry_path, current_depth - 1)
+
+
+def main(path: str):
+    for directory in GitProjectScanner(Path(path)):
+        print(directory)
+
+
+if __name__ == '__main__':
+    main('.')
