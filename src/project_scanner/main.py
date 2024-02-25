93042844befb440a9fffec4820e5219c7b23b0f1
Ivan Migunov <im@fckg.ru>
2024-02-25 15:46:06 +0300

    feat #3: tests

--- src/project_scanner/main.py
+++ src/project_scanner/main.py
@@ -1,49 +1,23 @@
 from pathlib import Path
 import os
-from typing import Iterator, Final
+from typing import Iterator
 
 
 class GitProjectScanner:
-    __slots__ = (
-        'path',
-        'depth',
-        'current_depth',
-        'found_directories',
-        'iterator_position',
-    )
-
-    path: Path
-    depth: int
-    current_depth: int
-    found_directories: list[str]
-    iterator_position: int
-
-    DEFAULT_DEPTH: Final[int] = 3
+    DEFAULT_DEPTH: int = 3
 
     def __init__(self, path: Path, depth: int = DEFAULT_DEPTH):
         self.path = path
         self.depth = depth
-        self.current_depth = 0
-        self.found_directories = []
-        self.iterator_position = 0
+        self.iterator = self.scan(self.path, self.depth)
 
     def __iter__(self) -> Iterator[str]:
-        self.found_directories = []
-        self.current_depth = self.depth
-        self.iterator_position = 0
-        return self
+        yield from self.scan(self.path, self.depth)
 
-    def __next__(self) -> str:
-        if not self.found_directories:
-            self.scan(self.path, self.depth)
-        if self.iterator_position < len(self.found_directories):
-            result = self.found_directories[self.iterator_position]
-            self.iterator_position += 1
-            return result
-        else:
-            raise StopIteration
+    def __next__(self):
+        return next(self.iterator)
 
-    def scan(self, current_path: Path, current_depth: int) -> list[str]:
+    def scan(self, current_path: Path, current_depth: int) -> Iterator[str]:
         """
         Осуществляет поиск вложенных Git проектов в заданном каталоге.
 
@@ -52,19 +26,17 @@ class GitProjectScanner:
         """
         if current_depth > 0:
             if '.git' in os.listdir(current_path):
-                self.found_directories.append(str(current_path))
+                yield str(current_path)
             for entry in os.listdir(current_path):
                 entry_path = current_path / entry
                 if entry_path.is_dir():
-                    self.scan(entry_path, current_depth - 1)
-        return self.found_directories
+                    yield from self.scan(entry_path, current_depth - 1)
 
 
-def main():
-    for directory in GitProjectScanner(Path('/Users/im/GitHub/')):
+def main(path: str):
+    for directory in GitProjectScanner(Path(path)):
         print(directory)
 
 
 if __name__ == '__main__':
-    for directory in GitProjectScanner(Path('/Users/im/GitHub/')):
-        print(directory)
+    main('.')
